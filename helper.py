e309b9328cc4a98b21e53127efe0c6b7abc29c66
malanmyri <malanmyri@gmail.com>
2023-04-10 16:16:14 +0200

    sdsd

--- helper.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-from system_class import*
-from particle_classes import*
-from matplotlib.patches import Rectangle
-
--- system_class.py
+++ system_class.py
@@ -34,7 +34,7 @@ class system:
         #Used in the calculation of convergence
         self.vel_curr = np.zeros((num_particles))
         self.vel_prior = np.ones((num_particles))* self.mean_velocity
-        self.tol = None
+        self.list_of_differences = []
         self.collision_length = []
 
     
@@ -62,30 +62,84 @@ class system:
                 self.particles.append(particle(positions[i], vel, self.radius[0], self.mass[1]))
     
     def convergence(self):
-        '''
-        Returns True if solution has converged
-        Returns False if solution has not converged
-        '''
-        i = 0
-        for p in self.particles:
-            self.vel_curr[i] = np.sqrt(p.velocity.vx**2 + p.velocity.vy**2)
-            i+=1
+        if len(self.mass) ==1:
+            i = 0
+            for p in self.particles:
+                self.vel_curr[i] = np.sqrt(p.velocity.vx**2 + p.velocity.vy**2)
+                i+=1
+            
+            sqr_prior = sum((np.ones(self.num_particles)*self.mean_velocity- self.vel_prior)**2 /self.num_particles)
+            sqr_curr =  sum((np.ones(self.num_particles)*self.mean_velocity- self.vel_curr)**2 /self.num_particles)
+
+            self.vel_prior = self.vel_curr.copy()
+            print(abs(sqr_prior - sqr_curr))
+            self.list_of_differences.append(abs(sqr_prior - sqr_curr))
         
-        sqr_prior = sum((np.ones(self.num_particles)*self.mean_velocity- self.vel_prior)**2 /self.num_particles)
-        sqr_curr =  sum((np.ones(self.num_particles)*self.mean_velocity- self.vel_curr)**2 /self.num_particles)
-
-        self.vel_prior = self.vel_curr.copy()
-        print(abs(sqr_prior - sqr_curr))
-        if abs(sqr_prior - sqr_curr) < self.tol: 
-            return True
-        else: 
-            return False
-
+        if len(self.mass) == 2:
+            i = 0
+            for p in self.particles:
+                self.vel_curr[i] = np.sqrt(p.velocity.vx**2 + p.velocity.vy**2)
+                i+=1
+            
+            sqr_prior_m_O2 = sum((np.ones(int(self.num_particles/2))*self.mean_velocity- self.vel_prior[:int(self.num_particles/2)])**2 /self.num_particles*2)
+            sqr_prior_4m_O2 = sum((np.ones(int(self.num_particles/2))*self.mean_velocity- self.vel_prior[int(self.num_particles/2):])**2 /self.num_particles*2)
 
 
+            sqr_curr_m_O2 = sum((np.ones(int(self.num_particles/2))*self.mean_velocity- self.vel_curr[:int(self.num_particles/2)])**2 /self.num_particles*2)
+            sqr_curr_4m_O2 = sum((np.ones(int(self.num_particles/2))*self.mean_velocity- self.vel_curr[int(self.num_particles/2):])**2 /self.num_particles*2)
 
 
-    
+            self.vel_prior = self.vel_curr.copy()
+            self.list_of_differences.append([abs(sqr_prior_m_O2 - sqr_curr_m_O2),abs(sqr_prior_4m_O2 - sqr_curr_4m_O2)])
+
+
+
+
+    def calculating_the_energy(self, number_of_updates,m_O2 ,average_max = 0.001, stopping_condition = 10):
+        '''
+        Input: 
+        system: The system we are calculated 
+        number_of_updates: The maximum number of updates before we stop the calculation 
+        average_max: Defines the step size over which we calculate the averages.
+        stopping_condition: the simulation will run untill the average number of particle-particle collisions per article reaches this number.
+        m_O2: mass of one oxygen molecule
+        Output: 
+
+        average kinetic energy over all particles
+        average kinetic energy over all particles weighing m_O2
+        average kinetic energy over all particles weighing 4*m_O2
+        '''
+
+        if len(self.mass) != 2: 
+            print("This function can only be used for systems with particles of different masses.")
+            return None
+
+        vel_m0 = np.zeros((int(self.num_particles/2), number_of_updates))
+        vel_4m0 = np.zeros((int(self.num_particles/2), number_of_updates))
+
+        vel = self.return_velocities()
+        vel_m0[:,0]=vel[:int(self.num_particles/2)]
+        vel_4m0[:,0]=vel[int(self.num_particles/2):]
+
+        average_current = self.average_particle_collision
+        average_next = average_current 
+        j = 1
+        while self.average_particle_collision < stopping_condition and j < number_of_updates:
+            while average_next-average_current < average_max: 
+                self.update_step()
+                average_next = self.average_particle_collision
+            
+            average_current = average_next
+            vel = self.return_velocities()
+            vel_m0[:,j]=vel[:int(self.num_particles/2)]
+            vel_4m0[:,j]=vel[int(self.num_particles/2):]
+            j +=1 
+
+        average_m_O2 = (m_O2*vel_m0**2/2).sum(axis = 0)
+        average_4m_O2= (4*m_O2*vel_4m0**2/2).sum(axis = 0)
+        average_all = average_m_O2 + average_4m_O2
+        return average_m_O2 , average_4m_O2 , average_all
+
     def crater(self):
         for num in range(self.num_particles):
             p = position(np.random.random_integers(self.radius[0], int((self.x_lim- self.radius[0])/self.radius[0]/2), 1)[0]*2*self.radius[0],np.random.random_integers(self.radius[0] ,int((self.y_lim- self.radius[0]-self.y_lim/2)/self.radius[0]/2), 1)[0]*self.radius[0]*2)
@@ -130,6 +184,9 @@ class system:
     def find_collisions(self):
         for p in self.particles: 
             self.find_collisions_particle(p)   
+    def find_collision_projectile(self):
+        self.find_collisions_particle(self.particles[-1]) #Because the last particle is the projectile
+
 
     def update_step(self):
         no_valid_collision = True
@@ -248,13 +305,21 @@ class system:
 
 
     def return_velocities(self):
-        vel = np.zeros(self.num_particles)
+        vel = np.zeros(len(self.particles))
         i = 0
         for p in self.particles:
             vel[i] = np.sqrt(p.velocity.vx**2 + p.velocity.vy**2)
             i+=1
         return vel
     
+    def energy(self):
+        vel = self.return_velocities()
+        energy_crater = (vel[:-1]**2).sum()*self.mass[0]/2    #This is the energy of all the small particles
+        projectile = self.particles[-1]
+        energy_projectile = vel[-1]**2 * projectile.mass /2   #energy of the projectile
+        return energy_crater + energy_projectile
+    
+
     def update(self,times):
         for time in range(times): 
             self.update_step()
